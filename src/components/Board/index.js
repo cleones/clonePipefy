fe078878bf09bd715c8d62898a984f0ae634b408
Dhiego Cleones <cleones06@gmail.com>
2020-04-21 11:15:24 -0300

    send to git

---
+++ src/components/Board/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import produce from 'immer';
+import { Container } from './styles';
+import BoardContext from './context';
+import List from '../List';
+import { loadLists } from '../../services/api';
+const data = loadLists();
+const Board = () => {
+  const [lists, setLists] = useState(data);
+
+  const move = (fromList, toList, from, to) => {
+    setLists(
+      produce(lists, (draft) => {
+        const dragged = draft[fromList].cards[from];
+        draft[fromList].cards.splice(from, 1);
+        draft[toList].cards.splice(to, 0, dragged);
+      })
+    );
+  };
+
+  return (
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => (
+          <List key={index} index={index} data={list} />
+        ))}
+      </Container>
+    </BoardContext.Provider>
+  );
+};
+
+export default Board;
